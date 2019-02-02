d2cfbaa6dc9e78a62b3c148d548a7f0eac1c27b0
Gareth Williams <gaz492@gmail.com>
2019-02-02 11:21:44 +0000

    Creative tabs

--- src/main/java/uk/gaz492/playerplates/PPItems.java
+++ src/main/java/uk/gaz492/playerplates/PPItems.java
@@ -1,38 +1,43 @@
 package uk.gaz492.playerplates;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.client.itemgroup.FabricCreativeGuiComponents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Settings;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import uk.gaz492.playerplates.item.DescriptiveBlockItem;
 
+import java.util.function.Supplier;
+
 public class PPItems {
 
-//    public static ItemGroup creativeTab = new ItemGroup(20, "player_plates") {
-//        @Override
-//        public ItemStack getIconItem() {
-//            return new ItemStack(Blocks.OAK_PRESSURE_PLATE);
-//        }
-//    };
+    public static Item obsidianPlate;
+    public static Item invisibleObsidianPlate;
 
-    private final Settings defaultSettings = new Settings().itemGroup(ItemGroup.REDSTONE);
+    public static Item mossyPlate;
+    public static Item invisibleMossyPlate;
 
-    public final Item obsidianPlate;
-    public final Item invisibleObsidianPlate;
+    public static ItemGroup creativeTab = FabricItemGroupBuilder.build(new Identifier("playerplates:player_plates"), new Supplier<ItemStack>() {
+        @Override
+        public ItemStack get() {
+            return new ItemStack(obsidianPlate);
+        }
+    });
 
-    public final Item mossyPlate;
-    public final Item invisibleMossyPlate;
+    private final Settings defaultSettings = new Settings().itemGroup(creativeTab);
 
     PPItems(PlayerPlates mod) {
-        this.obsidianPlate = this.register("obsidian_plate", mod.blocks.obsidianPlate, this.defaultSettings);
-        this.invisibleObsidianPlate = this.register("invisible_obsidian_plate", mod.blocks.invisibleObsidianPlate, this.defaultSettings);
+        obsidianPlate = this.register("obsidian_plate", mod.blocks.obsidianPlate, this.defaultSettings);
+        invisibleObsidianPlate = this.register("invisible_obsidian_plate", mod.blocks.invisibleObsidianPlate, this.defaultSettings);
 
-        this.mossyPlate = this.register("mossy_plate", mod.blocks.mossyPlate, this.defaultSettings);
-        this.invisibleMossyPlate = this.register("invisible_mossy_plate", mod.blocks.invisibleMossyPlate, this.defaultSettings);
+        mossyPlate = this.register("mossy_plate", mod.blocks.mossyPlate, this.defaultSettings);
+        invisibleMossyPlate = this.register("invisible_mossy_plate", mod.blocks.invisibleMossyPlate, this.defaultSettings);
     }
 
     private Item register (String id, Item item){
