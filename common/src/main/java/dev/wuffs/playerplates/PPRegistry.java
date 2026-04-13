7fd997d2ab03520728a9c10bc597d3044659ad59
Gareth Williams <gaz492@gmail.com>
2026-04-13 14:56:41 +0100

    Mikey broke it more

--- common/src/main/java/dev/wuffs/playerplates/PPRegistry.java
+++ common/src/main/java/dev/wuffs/playerplates/PPRegistry.java
@@ -1,8 +1,12 @@
 package dev.wuffs.playerplates;
 
+import dev.nanite.library.core.registry.NaniteRegistry;
+import dev.nanite.library.core.registry.RegistryHolder;
+import dev.nanite.library.platform.Platform;
 import dev.wuffs.playerplates.block.PlayerPlateBlock;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
@@ -25,17 +29,20 @@ public class PPRegistry {
         }
     }
 
-    public static final PlateEntry OBSIDIAN_PLATE = plate("obsidian_plate", PlayerPlateBlock.Sensitivity.PLAYER, false, Blocks.OBSIDIAN, BlockSetType.STONE);
-    public static final PlateEntry INVISIBLE_OBSIDIAN_PLATE = plate("invisible_obsidian_plate", PlayerPlateBlock.Sensitivity.PLAYER, true, Blocks.OBSIDIAN, BlockSetType.STONE);
+    public static final NaniteRegistry<Block> BLOCKS = NaniteRegistry.create(PlayerPlates.MOD_ID, BuiltInRegistries.BLOCK);
+    public static final NaniteRegistry<Item> ITEMS = NaniteRegistry.create(PlayerPlates.MOD_ID, BuiltInRegistries.ITEM);
 
-    public static final PlateEntry MOSSY_PLATE = plate("mossy_plate", PlayerPlateBlock.Sensitivity.ITEMS_MOBS, false, Blocks.MOSSY_COBBLESTONE, BlockSetType.STONE);
-    public static final PlateEntry INVISIBLE_MOSSY_PLATE = plate("invisible_mossy_plate", PlayerPlateBlock.Sensitivity.ITEMS_MOBS, true, Blocks.MOSSY_COBBLESTONE, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> OBSIDIAN_PLATE = plate("obsidian_plate", PlayerPlateBlock.Sensitivity.PLAYER, false, Blocks.OBSIDIAN, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> INVISIBLE_OBSIDIAN_PLATE = plate("invisible_obsidian_plate", PlayerPlateBlock.Sensitivity.PLAYER, true, Blocks.OBSIDIAN, BlockSetType.STONE);
 
-    public static final PlateEntry ITEM_PLATE = plate("item_plate", PlayerPlateBlock.Sensitivity.ITEMS, false, Blocks.STONE, BlockSetType.STONE);
-    public static final PlateEntry INVISIBLE_ITEM_PLATE = plate("invisible_item_plate", PlayerPlateBlock.Sensitivity.ITEMS, true, Blocks.STONE, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> MOSSY_PLATE = plate("mossy_plate", PlayerPlateBlock.Sensitivity.ITEMS_MOBS, false, Blocks.MOSSY_COBBLESTONE, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> INVISIBLE_MOSSY_PLATE = plate("invisible_mossy_plate", PlayerPlateBlock.Sensitivity.ITEMS_MOBS, true, Blocks.MOSSY_COBBLESTONE, BlockSetType.STONE);
 
-    public static final PlateEntry PASSIVE_MOB_PLATE = plate("passive_mob_plate", PlayerPlateBlock.Sensitivity.PASSIVE_MOB, false, Blocks.STONE, BlockSetType.STONE);
-    public static final PlateEntry INVISIBLE_PASSIVE_MOB_PLATE = plate("invisible_passive_mob_plate", PlayerPlateBlock.Sensitivity.PASSIVE_MOB, true, Blocks.STONE, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> ITEM_PLATE = plate("item_plate", PlayerPlateBlock.Sensitivity.ITEMS, false, Blocks.STONE, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> INVISIBLE_ITEM_PLATE = plate("invisible_item_plate", PlayerPlateBlock.Sensitivity.ITEMS, true, Blocks.STONE, BlockSetType.STONE);
+
+    public static final RegistryHolder<Block, PlayerPlateBlock> PASSIVE_MOB_PLATE = plate("passive_mob_plate", PlayerPlateBlock.Sensitivity.PASSIVE_MOB, false, Blocks.STONE, BlockSetType.STONE);
+    public static final RegistryHolder<Block, PlayerPlateBlock> INVISIBLE_PASSIVE_MOB_PLATE = plate("invisible_passive_mob_plate", PlayerPlateBlock.Sensitivity.PASSIVE_MOB, true, Blocks.STONE, BlockSetType.STONE);
 
     public static final List<PlateEntry> ALL_PLATES = List.of(
             OBSIDIAN_PLATE,
@@ -52,8 +59,9 @@ public class PPRegistry {
             .map(PlateEntry::item)
             .toList();
 
-    private static PlateEntry plate(String name, PlayerPlateBlock.Sensitivity sensitivity, boolean invisible, Block base, BlockSetType setType) {
+    private static RegistryHolder<Block, PlayerPlateBlock> plate(String name, PlayerPlateBlock.Sensitivity sensitivity, boolean invisible, Block base, BlockSetType setType) {
         Identifier id = Identifier.fromNamespaceAndPath(PlayerPlates.MOD_ID, name);
+        BLOCKS.register(id, () -> new PlayerPlateBlock(sensitivity, invisible, base, setType, name));
         PlayerPlateBlock block = new PlayerPlateBlock(sensitivity, invisible, base, setType, name);
         Item item = blockItem(id, block);
         return new PlateEntry(name, block, item);
